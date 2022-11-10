fc702b2a70b85f5d7086b0ba51a8bfa5fade3835
aam198 <aam198@psu.edu>
2022-11-10 15:41:34 -0500

    web-scraping examples

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer');
+// To save data to a file
+const fs = require('fs');
 
 
 async function run() {
@@ -18,8 +20,48 @@ async function run() {
   // console.log(html);
 
   // Evaluate the page object example
-  const title = await page.evaluate( () => document.title);
-  console.log(title);
+  // const title = await page.evaluate( () => document.title);
+  // console.log(title);
+
+  // See all text on the page
+  // const text = await page.evaluate (() => document.body.innerText);
+  // console.log(text);
+
+  //  Get all the links on a page
+  // Array.from() method that will create an array from an iterable object, this instance is .querySelectorAll node list
+  // the items in an array, takes 2 arguments, the second one is to grab the href element from <a>. 
+  // const links = await page.evaluate(() => Array.from(document.querySelectorAll('a'), (e) => e.href));
+  // console.log(links);
+
+  // To grab the specific course info, titles, links, have to inspect the devtools to see which one we want to see
+  // const courses = await page.evaluate(() => Array.from(document.querySelectorAll('#courses .card'), (e) => ({
+  //   // Properties we want returned
+  //   title: e.querySelector('.card-body h3').innerText,
+  //   level: e.querySelector('.card-body .level').innerText,
+  //   // To get the url
+  //   url: e.querySelector('.card-footer a').href,
+  //   // Getting text from the element
+  //   promo: e.querySelector('.card-footer .promo-code .promo').innerText
+  //   }))
+  // );
+
+  // Another Syntax instead of Array.from. 
+  const courses = await page.$$eval('#courses .card', (elements) => elements.map(e => ({
+    title: e.querySelector('.card-body h3').innerText,
+    level: e.querySelector('.card-body .level').innerText,
+   // To get the url
+    url: e.querySelector('.card-footer a').href,
+  // Getting text from the element
+    promo: e.querySelector('.card-footer .promo-code .promo').innerText
+  })))
+  console.log(courses);
+
+  // Save data to a JSON file with fs module, send in valid json by stringify
+  fs.writeFile('courses.json', JSON.stringify(courses), (err) => {
+    if (err) throw err;
+    console.log('File Saved');
+  })
+
 
   // Must have, close out the browser connection
   await browser.close();
