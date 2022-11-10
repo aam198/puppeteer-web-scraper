2ba2e86a5f67d8ce4b8e101494d54320bbd7a4eb
aam198 <aam198@psu.edu>
2022-11-10 14:41:01 -0500

    Inital build function examples for web scraping a site with nodeJS

--- index.js
+++ index.js
@@ -4,6 +4,25 @@ const puppeteer = require('puppeteer');
 async function run() {
   // Launch a browser, to access pages and different elements on a page
   const browser = await puppeteer.launch();
+  // Access a page
+  const page = await browser.newPage();
+  // which page to go to that you will be scraping, example temporary
+  await page.goto('https://traversymedia.com');
+
+  // Take a screenshot of the site/page, will overwrite
+  await page.screenshot({ path: 'example.png', fullPage: true });
+  await page.pdf({ path: 'example2.pdf', format: 'A4' });
+
+  // Get entire HTML of a page
+  // const html = await page.content();
+  // console.log(html);
+
+  // Evaluate the page object example
+  const title = await page.evaluate( () => document.title);
+  console.log(title);
+
+  // Must have, close out the browser connection
+  await browser.close();
 }
 
 // Run function
